872600d197fc31c73a36d747b20980cdd4b5dbf2
James Sapp <jameseisapp@gmail.com>
2021-01-18 21:39:23 -0600

    commit tree 19bac600ffb453d4b0969373f3185c764d08bd28

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,33 @@
+class ArtistsController < ApplicationController
+    def new
+        @artist = Artist.new 
+    end
+    
+    def create
+        @artist = Artist.create(artists_params)
+
+        redirect_to artist_path(@artist)
+    end
+
+    def edit
+        @artist = Artist.find(params[:id])
+    end
+    
+    def update
+        @artist = Artist.find(params[:id])
+        @artist.update(artists_params)
+
+        redirect_to artist_path(@artist)
+    end
+
+    def show
+        @artist = Artist.find(params[:id])
+    end
+
+    private
+
+    def artists_params
+        params.require(:artist).permit(:name, :bio)
+    end
+
+end
